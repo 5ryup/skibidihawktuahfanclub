700219df3d1e98b68c57a29b3ea638da85adcbc3
5ryup <lobsterskin0610@gmail.com>
2025-03-05 14:09:32 -0600

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -13,7 +13,7 @@ var games = [
   {
     id: 'eggycar',
     title: 'Eggy Car',
-    url: 'https://assets.3kh0.net/eggycar/index.html',
+    url: 'https://eggy-car.github.io/',
     image: 'https://assets.3kh0.net/eggycar/eggy-car.png'
   },
   {
