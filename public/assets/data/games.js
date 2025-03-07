93197932e278917fcf712138db4bb101bba8c578
5ryup <lobsterskin0610@gmail.com>
2025-03-07 12:18:34 -0600

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -26,6 +26,13 @@ var games = [
     id: 'cookieclicker',
     title: 'Cookie Clicker',
     description: 'Cookie Clicker is an addictive incremental game where players click to produce cookies, unlock upgrades, and aim to amass an ever-growing mountain of delicious digital treats.',
+    url: 'https://orteil.dashnet.org/cookieclicker/',
+    image: 'https://cdn.discordapp.com/icons/339131488111034389/73f15cd93d96504af9877a53816d110c.png'
+  },
+    {
+    id: 'cookieclicker(moddable)',
+    title: 'Cookie Clicker',
+    description: 'Cookie Clicker is an addictive incremental game where players click to produce cookies, unlock upgrades, and aim to amass an ever-growing mountain of delicious digital treats.',
     url: 'https://sushi8756.github.io/Cookie-Clicker-2.031/',
     image: 'https://cdn.discordapp.com/icons/339131488111034389/73f15cd93d96504af9877a53816d110c.png'
   },
